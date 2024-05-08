15062ace893cf8cbfa38e88edd852c424a64529a
Aarass <prokopovic75@gmail.com>
2024-05-08 14:57:03 +0200

    sva funkcionalnost je tu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Net;
 using System.Threading;
 
@@ -14,8 +12,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int worker_threads, completion_ports_threads;
-            ThreadPool.GetAvailableThreads(out worker_threads, out completion_ports_threads);
+            //int worker_threads, completion_ports_threads;
+            //ThreadPool.GetAvailableThreads(out worker_threads, out completion_ports_threads);
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://localhost:8080/");
             listener.Start();
@@ -23,24 +21,83 @@ namespace ConsoleApp1
             while (listener.IsListening)
             {
                 HttpListenerContext context = listener.GetContext();
-                string image_name = context.Request.RawUrl;
-                Console.WriteLine(image_name);
-            }
 
-            Image png_image = ConvertJpgToPng(Console.ReadLine());
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    HttpListenerRequest Request = context.Request;
+                    HttpListenerResponse Response = context.Response;
+
+                    string uri = context.Request.RawUrl;
+                    if (uri == "/favicon.ico") return;
+
+                    string path = $"..\\..\\assets{uri}";
+                    string newPath = $"{path.Substring(0, path.Length - 3)}png";
+
+                    Console.WriteLine(path);
+                    Console.WriteLine(newPath);
+
+                    bool alreadyConverted = File.Exists(newPath);
+                    if (!alreadyConverted)
+                    {
+                        Console.WriteLine("Converting image...");
+                        Image jpgImage;
+                        try
+                        {
+                            jpgImage = Image.FromFile(path);
+                        }
+                        catch (Exception)
+                        {
+                            SetErrorResponse(context, "Couldn't load jpg image");
+                            return;
+                        }
+
+                        try
+                        {
+                            jpgImage.Save(newPath, ImageFormat.Png);
+                        }
+                        catch (Exception)
+                        {
+                            SetErrorResponse(context, "Couldn't convert image");
+                            return;
+                        }
+                    }
+
+                    byte[] pngImage;
+                    try
+                    {
+                         pngImage = File.ReadAllBytes(newPath);
+                    }
+                    catch (Exception)
+                    {
+                        SetErrorResponse(context, "Couldn't load png image");
+                        return;
+                    }
+
+                    try
+                    {
+                        Response.ContentType = "image/png";
+                        Response.ContentLength64 = pngImage.Length;
+                        Response.OutputStream.Write(pngImage, 0, pngImage.Length);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Couldn't make response: {e.ToString()}");
+                    }
+                });
+            }
         }
 
-        public static Image ConvertJpgToPng(string image_name)
+        public static void SetErrorResponse(HttpListenerContext context, string error)
         {
-            string path = $"..\\..\\assets{image_name}";
-            Console.WriteLine(path);
-            string new_path = $"{path.Substring(0, path.Length - 3)}png";
-            Console.WriteLine(new_path);
+            Console.WriteLine(error);
+            HttpListenerResponse Response = context.Response;
 
-            Image jpg_image = Bitmap.FromFile(path);
+            byte[] html = Encoding.ASCII.GetBytes($"<p>{error}</p>");
 
-            jpg_image.Save(new_path, ImageFormat.Png);
-            return Bitmap.FromFile(new_path);
+            Response.ContentType = "text/html";
+            Response.ContentLength64 = html.Length;
+            Response.StatusCode = 400;
+            Response.OutputStream.Write(html, 0, html.Length);
         }
     }
 }

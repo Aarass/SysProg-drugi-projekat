17a03330e4d58b67714076bfcd717c96dfb08bf5
Aarass <prokopovic75@gmail.com>
2024-05-09 19:17:37 +0200

    dosta reimenovanja i klijent je zapocet

---
+++ Solution/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                Arguments = "",
+                FileName = @"C:\Users\proko\Documents\sisp\SysProg-prvi-projekat\Solution\WebServer\bin\Debug\ConsoleApp1.exe",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+            startInfo.WorkingDirectory =
+                @"C:\Users\proko\Documents\sisp\SysProg-prvi-projekat\Solution\WebServer\bin\Debug\";
+
+            Process process = Process.Start(startInfo);
+
+
+            while (true)
+            {
+                var request = new HttpClient().GetAsync($"http://localhost:8080/{Console.ReadLine()}");
+                request.Wait();
+                Console.WriteLine(request.Result.StatusCode);
+            }
+
+        }
+    }
+}
---
+++ Solution/WebServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current directory is {0}", Directory.GetCurrentDirectory());
+            WebServer server = new WebServer(8080);
+            server.RunModeChangeListener();
+            server.Run();
+        }
+    }
+}
